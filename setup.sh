10f9dacd54940d4e1e43d3064ffcd159253d2361
Hyperling <me@hyperling.com>
2023-02-17 19:45:29 -0600

    Add openSUSE for initial testing.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 branch="main"
 if [[ $1 != "" ]]; then
-  branch="$1"
+	branch="$1"
 fi
 
 os="$(cat /etc/os-release)"
@@ -11,33 +11,37 @@ os="$os $(uname -a)"
 
 echo "Making sure all necessary packages are installed..."
 if [[ `which ansible > /dev/null; echo $?` != 0 ]]; then
-  if [[ $os == *Debian* || $os == *Ubuntu* || $os == *"Pop!_OS"* || $os == *Mint* || $os == *Parrot* ]]; then
-    sudo apt update
-    sudo apt install -y ansible git <<< N
-    sudo mkdir -p /etc/ansible
-    sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
-  elif [[ $os == *FreeBSD* ]]; then
-    sudo pkg install -y py38-ansible git
-    sudo mkdir -p /usr/local/etc/ansible
-    sudo sh -c 'echo "localhost ansible_connection=local" > /usr/local/etc/ansible/hosts'
-  elif [[ $os == *Arch* || $os == *Manjaro* || $os == *Artix* ]]; then
-    sudo pacman -Sy --noconfirm ansible git
-    sudo mkdir -p /etc/ansible
-    sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
-  elif [[ $os == *Darwin* ]]; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "TESTING - EXIT!"
-    exit 0
-    brew install ansible git
-  elif [[ $os == *Fedora* ]]; then
-    sudo dnf install -y ansible git python3-libselinux
-    sudo mkdir -p /etc/ansible
-    sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
-  else
-    echo -e "ERROR: OS not detected."
-    echo -e "$os"
-    exit 1
-  fi
+	if [[ $os == *Debian* || $os == *Ubuntu* || $os == *"Pop!_OS"* || $os == *Mint* || $os == *Parrot* ]]; then
+		sudo apt update
+		sudo apt install -y ansible git <<< N
+		sudo mkdir -p /etc/ansible
+		sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
+	elif [[ $os == *FreeBSD* ]]; then
+		sudo pkg install -y py38-ansible git
+		sudo mkdir -p /usr/local/etc/ansible
+		sudo sh -c 'echo "localhost ansible_connection=local" > /usr/local/etc/ansible/hosts'
+	elif [[ $os == *Arch* || $os == *Manjaro* || $os == *Artix* ]]; then
+		sudo pacman -Sy --noconfirm ansible git
+		sudo mkdir -p /etc/ansible
+		sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
+	elif [[ $os == *Darwin* ]]; then
+		bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		echo "TESTING - EXIT!"
+		exit 0
+		brew install ansible git
+	elif [[ $os == *Fedora* ]]; then
+		sudo dnf install -y ansible git python3-libselinux
+		sudo mkdir -p /etc/ansible
+		sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
+	elif [[ $os == *openSUSE* ]]; then
+		sudo zypper install -y ansible git
+		sudo mkdir -p /etc/ansible
+		sudo sh -c 'echo "localhost ansible_connection=local" > /etc/ansible/hosts'
+	else
+		echo -e "ERROR: OS not detected."
+		echo -e "$os"
+		exit 1
+	fi
 fi
 echo "Installed!"
 
